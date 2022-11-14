e70cd51462f766b1095af7d47646d13ca76e712c
elmenda <jigarciah84@gmail.com>
2022-11-14 10:55:13 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
-const ExampleProps = ({hero, isNobelWinner, rating, title}) => { 
+const ExampleProps = ({ hero, isNobelWinner, rating, title }) => {
   return (
-    <div>{title}
-    <p>Name: {hero.name}</p>
-    <p>Rating: {rating}</p>
-    <p>Superpowers: {hero.superPowers.join('/')}</p>
-    {isNobelWinner && <p>Has won the nobel price</p>}
+    <div>
+      {title}
+      <p>Name: {hero.name}</p>
+      <p>Rating: {rating}</p>
+      <p>Superpowers: {hero.superPowers.join('/')}</p>
+      {isNobelWinner && <p>Has won the nobel price</p>}
     </div>
-  )
-}
+  );
+};
 
 export default function App() {
   return (
-    //Solo puede tener un padre. Una solucion a intentar mostrar estos dos parrafor seria usar un div o React.Fragment
-    <React.Fragment>
+    //Solo puede tener un padre. Una solucion a intentar mostrar estos dos parrafor seria usar:
+    // div
+    //React.Fragment
+    // <> ... </>
+
+    // <div>
+    //   <p>Algo</p>
+    //   <p>Algo mas</p>
+    // </div>
+    // <React.Fragment>
+    //   <p>Algo</p>
+    //   <p>Algo mas</p>
+    // </React.Fragment>
+    <>
       <p>Algo</p>
       <p>Algo mas</p>
-    </React.Fragment>
+    </>
   );
 }
